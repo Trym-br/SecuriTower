c132ab9b164b46e6d075b84a84fb8d75a6484b1a
Trym-br <trym.brandal.arhaug@gmail.com>
2025-03-17 13:44:35 +0100

    Crystals V2

--- Assets/Scripts/Crystal/CrystalController.cs
+++ Assets/Scripts/Crystal/CrystalController.cs
@@ -9,12 +9,26 @@ using UnityEngine.InputSystem;
 public class CrystalController : MonoBehaviour
 {
     [SerializeField] private Vector3[] InputPoints;
-    [SerializeField] private bool[]    ActiveInputPoints;
     [SerializeField] private Vector3[] OutputPoints;
+    [SerializeField] private bool[]    ActiveInputs;
+    [SerializeField] private bool isSender = false;
     public float Rotation;
+    
+    private bool isLaserOn = true;
+    private GameObject[] LastHits;
 
-    private void SendLaser(Vector3 OutputPoint, bool isON)
+    private void Awake()
     {
+        ActiveInputs = new bool[InputPoints.Length];
+        Array.Fill(ActiveInputs, isSender);
+        
+        LastHits = new GameObject[InputPoints.Length];
+        Array.Fill(LastHits, null);
+    }
+
+    private void SendLaser(int OutputPointIndex, bool isOn)
+    {
+        Vector3 OutputPoint = OutputPoints[OutputPointIndex];
         Vector3 origin = transform.TransformPoint(OutputPoint);
         Vector3 dir = origin - transform.position;
         // print("Laser emitting in dir: " + dir);
@@ -26,46 +40,69 @@ public class CrystalController : MonoBehaviour
         if (hit)
         {
             // print("Hit: " + hit.collider.name);
-            Debug.DrawLine(origin, hit.point, Color.green);
+            
+            // If lost LOS on crystal, disable it
+            
+            print("Hit: " + hit.collider.name + " / " + LastHits[OutputPointIndex]);
+            if (LastHits[OutputPointIndex] != null && hit.collider.gameObject != LastHits[OutputPointIndex])
+            {
+                print("UMBRELLA CASE");
+                LastHits[OutputPointIndex].GetComponentInChildren<CrystalController>().OnLaserHitPoint(hit.point, false, true);
+                LastHits[OutputPointIndex] = null;
+            }
+            // If hit crystal, enable it
             if (hit.collider.CompareTag("Crystal"))
             {
-               hit.collider.gameObject.GetComponent<CrystalController>().OnLaserHitPoint(hit.point);
+               hit.collider.gameObject.GetComponentInChildren<CrystalController>().OnLaserHitPoint(hit.point, isOn);
+               LastHits[OutputPointIndex] = hit.collider.gameObject;
             }
+            
+            Debug.DrawLine(origin, hit.point, Color.green);
         }
         Debug.DrawRay(origin, dir, Color.red);
     }
-
-    public void OnLaserHitDir(Vector3 direction)
-    {
-        for (int i = 0; i < InputPoints.Length; i++)
-        {
-            Vector3 origin = transform.TransformPoint(InputPoints[i]);
-            Vector3 dir = origin - transform.position;
-        }
-    }
     
-    public void OnLaserHitPoint(Vector3 hitPoint)
+    public void OnLaserHitPoint(Vector3 hitPoint, bool isOn, bool forceTrue = false)
     {
         for (int i = 0; i < InputPoints.Length; i++)
         {
-            print("hitPoint/InputPoint: " + hitPoint + " / "  + InputPoints[i]);
-            if (hitPoint == transform.TransformPoint(InputPoints[i]))
+            if (hitPoint == transform.TransformPoint(InputPoints[i]) || forceTrue)
             {
-                ActiveInputPoints[i] = true;
+                ActiveInputs[i] = isOn;
             }
         }
+        // Cut Down on Delay
+        CrystalLogic();
     }
 
+    private void CrystalLogic()
+    {
+        bool shootFlag = ActiveInputs.All(x=>x);
+        if (shootFlag)
+        {
+            isLaserOn = true;
+            for (int i = 0; i < OutputPoints.Length; i++)
+            {
+                SendLaser(i, true);
+            }
+        }
+        else if (isLaserOn)
+        {
+            for (int i = 0; i < OutputPoints.Length; i++)
+            {
+                SendLaser(i, false);
+            }
+            isLaserOn = false;
+        }
+    }
     private void FixedUpdate()
     {
-        for (int i = 0; i < OutputPoints.Length; i++)
-        {
-            SendLaser(OutputPoints[i], true);
-        }
+        CrystalLogic();
     }
 
     private void OnDrawGizmos()
     {
+        // Drawing input & output Points
         foreach (var Point in InputPoints)
         {
             Gizmos.color = new Color(0, 1, 0, 0.5f);
