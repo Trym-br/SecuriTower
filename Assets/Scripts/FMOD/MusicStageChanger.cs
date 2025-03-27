4c61c87fad9867ab05103845d15dd53fc7408b61
arena-allocator-abuser <rogopa6540@degcos.com>
2025-03-27 14:19:50 +0100

    Music stage changer

---
+++ Assets/Scripts/FMOD/MusicStageChanger.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MusicStageChanger : MonoBehaviour {
+	// For the main menu.
+	public void TitleScreenMusic() {
+		if (FMODController.instance == null) {
+			Debug.LogError("Trying to set the music stage to 'Title Screen Music', but the '{nameof(FMODController)}' is not loaded!");
+		} else {
+			FMODController.instance.currentMusicStage = FMODController.MusicStage.TitleScreen;
+		}
+	}
+
+	// For the intro cutscene.
+	public void IntroCutsceneMusic() {
+		if (FMODController.instance == null) {
+			Debug.LogError("Trying to set the music stage to 'Intro Cutscene', but the '{nameof(FMODController)}' is not loaded!");
+		} else {
+			FMODController.instance.currentMusicStage = FMODController.MusicStage.IntroCutscene;
+		}
+	}
+
+	// For all the levels.
+	public void MainStageMusic() {
+		if (FMODController.instance == null) {
+			Debug.LogError("Trying to set the music stage to 'Main Stage', but the '{nameof(FMODController)}' is not loaded!");
+		} else {
+			FMODController.instance.currentMusicStage = FMODController.MusicStage.MainStage;
+		}
+	}
+}
