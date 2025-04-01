c10a20e067c8cf5a50e6e1d0121fe1ef63b0edf1
arena-allocator-abuser <rogopa6540@degcos.com>
2025-04-01 10:09:02 +0200

    Functions for UI

--- Assets/Scripts/FMOD/MusicStageChanger.cs
+++ Assets/Scripts/FMOD/MusicStageChanger.cs
@@ -10,7 +10,6 @@ public class MusicStageChanger : MonoBehaviour {
 		}
 	}
 
-	// For the intro cutscene.
 	public void IntroCutsceneMusic() {
 		if (FMODController.instance == null) {
 			Debug.LogError($"Trying to set the music stage to 'Intro Cutscene', but the '{nameof(FMODController)}' is not loaded!");
@@ -25,6 +24,23 @@ public class MusicStageChanger : MonoBehaviour {
 			Debug.LogError($"Trying to set the music stage to 'Main Stage', but the '{nameof(FMODController)}' is not loaded!");
 		} else {
 			FMODController.instance.currentMusicStage = FMODController.MusicStage.MainStage;
+		}
+	}
+
+	// For the final cutscene where the music cuts.
+	public void WaitingSpaceMusic() {
+		if (FMODController.instance == null) {
+			Debug.LogError($"Trying to set the music stage to 'Waiting Space', but the '{nameof(FMODController)}' is not loaded!");
+		} else {
+			FMODController.instance.currentMusicStage = FMODController.MusicStage.WaitingSpace;
+		}
+	}
+
+	public void CreditsMusic() {
+		if (FMODController.instance == null) {
+			Debug.LogError($"Trying to set the music stage to 'Credits', but the '{nameof(FMODController)}' is not loaded!");
+		} else {
+			FMODController.instance.currentMusicStage = FMODController.MusicStage.Credits;
 		}
 	}
 }
